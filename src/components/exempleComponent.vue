50ea9f9dd8742348c65b40361f34a1cfe8708827
juniordev237 <juniorndono21@gmail.com>
2021-09-04 14:14:45 +0100

    avancé du développement du dernier item splide-slide du composant exempleComponent.vue

--- src/components/exempleComponent.vue
+++ src/components/exempleComponent.vue
@@ -1,85 +1,81 @@
 <template>
-            <div class="">
-                <splide :options="options" class="container">
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
-                        <div class="content">
-                            <h1>Product Design</h1>
-                            <h3>UI/UX, Design</h3>
+    <div class="container">
+        <splide :options="options" class="">
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
+                <div class="content">
+                    <h1>Product Design</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
+                <div class="content">
+                    <h1>Custom Website</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png">
+                <div class="content">
+                    <h1>Digital Marketing</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
+                <div class="content">
+                    <h1>Digital Marketing</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
+                <div class="content">
+                    <h1>Digital Marketing</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png">
+                <div class="content">
+                    <h1>Digital Marketing</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
+                <div class="content">
+                    <h1>Digital Marketing</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
+                <div class="content">
+                    <h1>Digital Marketing</h1>
+                    <h3>UI/UX, Design</h3>
+                </div>
+            </splide-slide>
+            <splide-slide>
+                <div class="image">
+                    <img src="../assets/aveiroDjess.jpg" class="object-cover">
+                </div>
+                <div class="content border-2 rounded-b-2xl">
+                    <h1 class="mx-2 mt-2 text-bold text-left text-blue-500 text-tiny border-b font-light border-gray-300">Digital Marketing</h1>
+                    <div class="">
+                        <div class="__icons ">
+                            <ul class="recipe-details">
+                                <li class="recipe-details-item time"><img src="../assets/icones/eye.png" class="inline h-3 w-3  mb-1"><span class="value">999</span><span class="title">views</span></li>
+                                <li class="recipe-details-item ingredients"><img src="../assets/icones/like.png" class="inline h-3 w-3 mb-1"><span class="value">5</span><span class="title">likes</span></li>
+                                <li class="recipe-details-item servings"><img src="../assets/icones/share.png" class="inline h-3 w-3 mb-1"><span class="value">48</span><span class="title">shares</span></li>
+                            </ul>
                         </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
-                        <div class="content">
-                            <h1>Custom Website</h1>
-                            <h3>UI/UX, Design</h3>
-                        </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png">
-                        <div class="content">
-                            <h1>Digital Marketing</h1>
-                            <h3>UI/UX, Design</h3>
-                        </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
-                        <div class="content">
-                            <h1>Digital Marketing</h1>
-                            <h3>UI/UX, Design</h3>
-                        </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
-                        <div class="content">
-                            <h1>Digital Marketing</h1>
-                            <h3>UI/UX, Design</h3>
-                        </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png">
-                        <div class="content">
-                            <h1>Digital Marketing</h1>
-                            <h3>UI/UX, Design</h3>
-                        </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
-                        <div class="content">
-                            <h1>Digital Marketing</h1>
-                            <h3>UI/UX, Design</h3>
-                        </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
-                        <div class="content">
-                            <h1>Digital Marketing</h1>
-                            <h3>UI/UX, Design</h3>
-                        </div>
-                    </splide-slide>
-                    <splide-slide>
-                        <div class="image w-auto h-auto relative">
-                            <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png" class="shadow-md">
-                            <div class="footer_card bg-pink-600 opacity-50 h-3/5 w-4 shadow-lg absolute top-0 left-0 z-10"></div>
-                        </div>
-                        <div class="content border-2 rounded-b-2xl">
-                            <h1 class="mx-2 mt-2 text-bold text-left text-blue-500 text-tiny border-b font-light border-gray-300">Digital Marketing</h1>
-                            <div class="">
-                                <div class="__icons ">
-                                    <ul class="recipe-details">
-                                        <li class="recipe-details-item time"><img src="../assets/icones/eye.png" class="inline h-3 w-3  mb-1"><span class="value">999</span><span class="title">views</span></li>
-                                        <li class="recipe-details-item ingredients"><img src="../assets/icones/like.png" class="inline h-3 w-3 mb-1"><span class="value">5</span><span class="title">likes</span></li>
-                                        <li class="recipe-details-item servings"><img src="../assets/icones/share.png" class="inline h-3 w-3 mb-1"><span class="value">48</span><span class="title">shares</span></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </splide-slide>
-                </splide>
-            </div>
-                
-            
-
+                    </div>
+                </div>
+            </splide-slide>
+        </splide>
+    </div>
 </template>
 
 <script>
@@ -93,14 +89,21 @@ export default {
     components: {
         Splide, SplideSlide, itemExemple
     },
-     setup() {
+    setup() {
         let options = {
             type: "loop",
-            perPage: 3,
+            perPage: 4,
 	        rewind : true,
+            // breakpoints: {
+            //     640: {
+            //         perPage: 1,
+            //         fixedWidth: 200,
+            //         fixedHeight: 200
+            //     }
+            // }
         }
         return {
-			  options
+			options
         }
     },
 
@@ -115,70 +118,93 @@ export default {
 }
 </script>
 
-<style lang="postcss">
+<style lang="scss">
+
+    %resize {
+        height: 120px;
+        width: 180px;
+    }
     .recipe-details {
-        @apply flex list-none py-1 px-1 m-0 justify-between 
+        display: flex;
+        list-style: none;
+        padding: .3rem;
+        margin: 0;
+        justify-content: space-around;
+
+        .recipe-details-item {
+            display: flex-1;
+            cursor: pointer;
+            vertical-align: middle;
+            border-bottom: solid 0 transparent;
+            background-color: transparent;
+            border-radius: 0;
+            padding: 0 4px;
+            width:auto;
+            transition: transform .5s, border-bottom .5s, background-color .5s, border-radius .5s, transform .5s;
+            
+
+            &:hover {
+                border-bottom: solid 1px rgba(68, 174, 245, 0.959);
+                background-color: white;
+                border-radius: 10px;
+                transform: scale(1.1)
+            }
+        }
+
+        i {
+            font-size: 30px;
+        }
     }
 
-    .recipe-details-item {
-        @apply flex-1 cursor-pointer  align-middle 
-    }
-
-    .recipe-details-item {
-        border-bottom: solid 0 transparent;
-        background-color: transparent;
-        border-radius: 0;
-        transition: border-bottom .5s, background-color .5s, border-radius .5s, transform .5s;
-        
-    }
-
-    .recipe-details-item:hover {
-        border-bottom: solid 1px rgba(68, 174, 245, 0.959);
-        background-color: white;
-        border-radius: 10px;
-    }
-
-    .recipe-details-item i {
-        font-size: 30px;
-    }
 
     .value {
-        @apply text-blue-500 ml-1 align-bottom text-xs font-thin
+        color: rgba(59, 131, 246, 0.952);
+        margin-left: .25rem;
+        vertical-align: bottom;
+        font-size: .75rem;
+        font-weight: 100;
     }
 
     .title {
-        @apply block -mt-2 text-black font-light pt-1 
-    }
-
-    .title {
+        display: block;
+        margin-top: -0.5rem;
+        color: black;
+        font-weight: 300;
+        padding-top: .25rem;
         font-size: .57em
     }
+
 
     img.inline {
         opacity: .8
     }
 
-    .footer_card {
-        margin: auto;
-    }
-
     .image {
+        @extend %resize;
         cursor: pointer;
         background-color: transparent;
         transition: background-color .5s;
+        // border: 1px solid green;
+
+        &:hover {
+            background-color: #1f1f1f;
+        }
+
+        img {
+            @extend %resize;
+            opacity: 1;
+            transition: opacity .5s
+        }
+
+        img:hover {
+            opacity: .35
+        }
     }
 
-    .image:hover {
-        background-color: rgb(31, 31, 31);
+    .content {
+        @extend %resize;
+        height: 77px
     }
 
-    .image img {
-        opacity: 1;
-        transition: opacity .5s
-    }
-
-    .image img:hover {
-        opacity: .35
-    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
-// import Vue from 'vue'
 import App from './App.vue'
 import './index.css'
-// import EasySlider from 'vue-easy-slider'
-
-// Vue.use(EasySlider)
 
 
 createApp(App).mount('#app')
