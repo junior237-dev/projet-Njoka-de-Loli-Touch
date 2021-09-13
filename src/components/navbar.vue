6871f03d80036767c0e44a298458e57d063c712c
juniordev237 <juniorndono21@gmail.com>
2021-09-13 07:35:50 +0100

    modifications apportées dans le composant navbar.vue: application de la classe .active sur les éléments de la navbar sur lesquels on clique

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,31 +1,31 @@
 <template>
     <nav class="navbar">
         <div class="py-2">
-            <a href="#">
+            <a href="#" @click.stop="activeClass">
                 <svg class="icon">
                     <use xlink:href="../assets/icones/sprite.svg#home"></use>
                 </svg>
                 Accueil
             </a>
-            <a href="#" >
+            <a href="#" @click.stop="activeClass">
                 <svg class="icon">
                     <use xlink:href="../assets/icones/sprite.svg#menu (1)"></use>
                 </svg>
                 Catégories
             </a>
-            <a href="#">
+            <a href="#" @click.stop="activeClass">
                 <svg class="icon">
                     <use xlink:href="../assets/icones/sprite.svg#add-to-cart"></use>
                 </svg>
                 Panier
             </a>
-            <a href="#">
+            <a href="#" @click.stop="activeClass">
                 <svg class="icon">
                     <use xlink:href="../assets/icones/sprite.svg#music-note-on-folder"></use>
                 </svg>
                 Ma musique
             </a>
-            <a href="#" :class="Active">
+            <a href="#" class="active" @click.stop="activeClass">
                 <svg class="icon">
                     <use xlink:href="../assets/icones/sprite.svg#profile-user"></use>
                 </svg>
@@ -38,33 +38,34 @@
 <script>
 export default {
     name: "navbar",
-    setup () {
-
-       /* "Active" est la propriété que j'utilise
-        *pour habiller un élément de la navbar 
-         *sur lequel on clique */
-
-       let Active = [
-                "bg-gray-100", 
-                "mb-5", 
-                "shadow-md",
-                "opacity-100",
-                "px-1",
-                "py-1",
-                "h-auto",
-                "text-blue-300",
-                "rounded-none"   
-            ]
-
-
-       return {
-           Active
+    setup (props, context) {
+        console.log()
+        return {
+        
+       }
+   },
+   mounted () {
+       let links = document.querySelectorAll('a')
+   },
+   methods: {
+       activeClass (e) {
+            let parent = e.currentTarget.parentNode
+            if (e.currentTarget.classList.contains("active")) {
+                return 
+            }
+            parent.querySelector("a.active").classList.remove("active")
+            e.currentTarget.classList.add('active')
+           
        }
    }
 }
 </script>
 
 <style lang="postcss">
+
+    .active {
+        @apply bg-gray-100 mb-5 shadow-md opacity-100 p-1 h-auto text-blue-300 rounded-none
+    }
     .navbar {
         @apply fixed z-50 bottom-0  h-16 w-full  pt-1  bg-white  border-t
     }
