355708ad567f5fad69cd3deb77770bcfd07cfaea
juniordev237 <juniorndono21@gmail.com>
2021-09-16 08:07:31 +0100

    modifications apportées sur postAlbum.vue et développement partiel du composant en question

--- src/components/postAlbum.vue
+++ src/components/postAlbum.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="post-album">
+    <div class="post-album max-w-xl mx-auto">
         <div class="header flex flex-row justify-between w-full px-4  h-16">
             <span class="back inline-block text-3xl text-black py-2">
                 <i class="bi bi-arrow-left"></i>
@@ -11,25 +11,23 @@
             </ul>
         </div>
         <div class="post-description mt-5">
-            <div class="image-description inline-block w-1/2 align-bottom">
-                <img src="../assets/Maleya.jpg" alt="" class="object-cover shadow-md rounded-sm">
-            </div>
-            <div class="description inline-block w-1/2 h-auto align-bottom">
-                <h1 class="text-left text-2xl font-semibold w-5/6 mx-auto text-black">Cardio</h1>
-                <h3 class="text-left w-5/6 mx-auto -mt-2">X-maleya</h3>
-                <div class="informations text-blue-500 text-2xl w-5/6 mx-auto">
-                    <ul class="info-1 flex flex-row flex-wrap justify-between">
-                        <li class="w-16 h-16 border bg-black py-2"><i class="bi bi-eye-fill"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
-                        <li class="w-16 h-16 border bg-black py-2"><i class="bi bi-hand-thumbs-up-fill"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
-                        <li class="w-16 h-16 border bg-black py-2"><i class="bi bi-share-fill"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
-                        <li class="w-16 h-16 border bg-black py-2"><i class="bi bi-music-note-list"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
-                        <li class="w-16 h-16 border bg-black py-2"><i class="bi bi-clock"></i><span class="value text-sm  font-bold block -mt-2">20mn</span></li>
-                        <li class="w-16 h-16 border bg-black py-2"><i class="bi bi-cash-coin"></i><span class="value text-sm font-bold block -mt-2">200XAF</span></li>
+            <h1 class="text-left text-2xl font-semibold  mx-auto text-black">Cardio</h1>
+            <h3 class="text-left mx-auto -mt-2">X-maleya</h3>  
+            <img src="../assets/Maleya.jpg" alt="" class="block w-full max-h-80 object-cover">
+            <div class="description inline-block w-full h-full align-bottom">
+                <div class="informations text-blue-500 text-2xl pt-1 w-full mx-auto">
+                    <ul class="info-1 grid grid-cols-3">
+                        <li class="w-full  border bg-black py-2"><i class="bi bi-eye-fill"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
+                        <li class="w-full  border bg-black py-2"><i class="bi bi-hand-thumbs-up-fill"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
+                        <li class="w-full  border bg-black py-2"><i class="bi bi-share-fill"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
+                        <li class="w-full  border bg-black py-2"><i class="bi bi-music-note-list"></i><span class="value text-tiny font-bold block -mt-2">20</span></li>
+                        <li class="w-full  border bg-black py-2"><i class="bi bi-clock"></i><span class="value text-sm  font-bold block -mt-2">20mn</span></li>
+                        <li class="w-full  border bg-black py-2"><i class="bi bi-cash-coin"></i><span class="value text-sm font-bold block -mt-2">200XAF</span></li>
                     </ul>
                     <div class="control">
-                        <button class="btn w-full bg-blue-500 rounded-sm h-12 mt-1">
+                        <button class="btn w-full bg-blue-500 rounded-sm h-20 mt-1">
                             <span class="shopping-cart text-white"><i class="bi bi-cart3"></i></span>
-                            <span class="buy text-white ml-3">Get now</span>
+                            <span class="buy text-white ml-3">Obtenez maintenant</span>
                         </button>
                     </div>
                 </div>
@@ -37,6 +35,17 @@
         </div>
     </div>
 </template>
+
+<script>
+import fake from "./fake.vue"
+export default {
+    name: 'postAlbum',
+    components: {fake},
+    setup() {
+        
+    },
+}
+</script>
 
 <style scoped>
     .header {
