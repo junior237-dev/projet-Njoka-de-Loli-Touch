2b40154155d797c2a8a6216e53e5d64d79481e3b
juniordev237 <juniorndono21@gmail.com>
2021-09-27 18:43:31 +0100

    création du composant categoryElement.vue et son introduction dans category.vue

--- src/components/category.vue
+++ src/components/category.vue
@@ -1,12 +1,9 @@
 <!-- voici le composant "categorie" que j'ai commencé -->
 <template>
-    <div class="max-w-lg mx-auto min-h-screen bg-blue-500 pt-16"> <!-- son contenant principal dont j'ai mis le background en bleu -->
+    <div class="max-w-lg mx-auto min-h-screen bg-blue-500 pt-20 px-3"> <!-- son contenant principal dont j'ai mis le background en bleu -->
         <search-bar :classApply="['z-10','right-0','left-0','top-0','fixed', 'shadow-md','bg-white']"/>
-        <div class="categories w-5/6 mx-auto grid grid-cols-2 gap-2">
-            <div class="category-element w-full h-24 shadow-lg mb-1 bg-white" v-for="item in 18" :key="item">
-                {{item}}
-                <div class="illustration w-2/3 bg-yellow-600 float-right h-full  rounded-tl-full rounded-r-2xl shadow-lg"></div>
-            </div>
+        <div class="categories w-full mx-auto grid grid-cols-2 gap-2">
+            <category-element />
         </div>
     </div>
 </template>
@@ -18,15 +15,14 @@
  * et enlever ce commentaire si tout est ok
  * et laisser juste la dernière ligne
  * import category-element from "./category-element.vue" */
- import searchBar from './searchBar.vue'
+import searchBar from './searchBar.vue'
+import categoryElement from './categoryElement.vue'
  
 export default {
     name: "category",
     components: {
-        //category-element,
+        categoryElement,
         searchBar
     }
 }
 </script>
-
-
---
+++ src/components/categoryElement.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="category-element w-full h-28 rounded-3xl mb-1 bg-red-700" v-for="item in 18" :key="item">
+        <div class="title-category w-2/5 float-left pl-3 pt-2">
+            <span class="align-top text-white font-semibold text-sm leading-none">
+                Bikutsi du Kmer
+            </span>
+        </div>
+        <div class="illustration w-3/5 bg-yellow-700 float-right h-full shadow-lg">
+            <img src="../assets/jovi.jpg" alt="tenor" class="object-cover w-full h-full" >
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "categoryElement"
+}
+</script>
+<style>
+    .illustration, .illustration img {
+        border-top-left-radius: 50%;
+        border-bottom-left-radius: 50%;
+        border-top-right-radius: 24px;
+        border-bottom-right-radius: 24px;
+    }
+
+    .category-element {
+        box-shadow: 0 1px 8px black;
+    }
+</style>
